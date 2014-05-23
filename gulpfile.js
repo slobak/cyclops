e5bc394397974386d7c842b1d2d4842afcf1c2e0
Greg Slovacek <slobak@gmail.com>
2014-05-23 00:52:13 -0700

    Basic functioning version, packaging is hacky

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var gulp = require("gulp");
 
 // Include Our Plugins
 var jshint = require("gulp-jshint");
-var sass = require("gulp-sass");
 var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
-var wrap = require("gulp-wrap-umd");
+var wrap = require("gulp-wrap");
+var wrapumd = require("gulp-wrap-umd");
+var replace = require("gulp-replace");
 
 // Lint Task
 gulp.task("lint", function() {
@@ -16,31 +17,21 @@ gulp.task("lint", function() {
       .pipe(jshint.reporter("default"));
 });
 
-// Compile Our Sass
-gulp.task("sass", function() {
-  return gulp.src("scss/*.scss")
-      .pipe(sass())
-      .pipe(gulp.dest("css"));
-});
-
 // Concatenate & Minify JS
-gulp.task("scripts", function() {
-  return gulp.src("lib/*.js")
+gulp.task("js", function() {
+  return gulp.src(["lib/**/*.js", "index.js"])
+      .pipe(replace(/var .* = require.*;/g, ""))
+      .pipe(replace(/module.exports = .*;/g, ""))
       .pipe(concat("all.js"))
-      .pipe(wrap({
-        namespace: "Asana.cyclops"
-      }))
+      .pipe(wrap("(function() {<%= contents %>\nreturn exports;\n})()"))
+      .pipe(wrapumd({ namespace: "cyclops" }))
       .pipe(gulp.dest("dist"))
-      .pipe(rename("all.min.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest("dist"));
 });
 
 // Watch Files For Changes
 gulp.task("watch", function() {
-  gulp.watch("lib/*.js", ["lint", "scripts"]);
-  gulp.watch("scss/*.scss", ["sass"]);
+  gulp.watch("lib/**/*.js", ["lint", "js"]);
 });
 
 // Default Task
-gulp.task("default", ["lint", "sass", "scripts", "watch"]);
+gulp.task("default", ["lint", "js", "watch"]);
--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 var Cyclops = require("./lib/cyclops");
 
 /**
- *
- * Record event
- *
+ * var cyclops = require("cyclops").init({
+ *   env: {
+ *     now: ...
+ *     setTimeout: ...
+ *   },
+ *   recorder: {
+ *     buffer: true,
+ *     max_buffer_time_ms: 1000,
+ *     max_buffer_events: 100
+ *   }
+ * });
  *
  * cyclops.onEvents(function(events) {
- *   // ...
+ *
  * });
  *
  * cyclops.config();
  *
- * cyclops.start()
+ * cyclops.start();
  *
- * cyclops.event();
+ * cyclops.event({
+ *   type: "my_custom_event",
+ *   foo: "bar"
+ * });
+ * cyclops.flush();
  *
  * cyclops.stop();
- *
  *
  */
 
 exports.init = function(options) {
-  return new Cyclops(options);
+  return Cyclops.init(options);
 };
-
