47818b95473b666209779d6a90efb27c49b028be
Greg Slovacek <slobak@gmail.com>
2014-05-23 16:54:42 -0700

    Completed both browser modules and server launcher, not tested in new organization though

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Include gulp
 var gulp = require("gulp");
 
@@ -9,6 +11,7 @@ var rename = require("gulp-rename");
 var wrap = require("gulp-wrap");
 var wrapumd = require("gulp-wrap-umd");
 var replace = require("gulp-replace");
+var browserify = require("gulp-browserify");
 
 // Lint Task
 gulp.task("lint", function() {
@@ -17,16 +20,32 @@ gulp.task("lint", function() {
       .pipe(jshint.reporter("default"));
 });
 
+//var buildBrowserJs = function(stream, filename) {
+//  return stream.pipe(replace(/var .* = require.*;/g, ""))
+//      .pipe(replace(/module.exports = .*;/g, ""))
+//      .pipe(concat(filename))
+//      .pipe(wrap("(function() {<%= contents %>\nreturn exports;\n})()"))
+//      .pipe(wrapumd({ namespace: "cyclops" }))
+//      .pipe(gulp.dest("dist"));
+//};
+//
 // Concatenate & Minify JS
-gulp.task("js", function() {
-  return gulp.src(["lib/**/*.js", "index.js"])
-      .pipe(replace(/var .* = require.*;/g, ""))
-      .pipe(replace(/module.exports = .*;/g, ""))
-      .pipe(concat("all.js"))
-      .pipe(wrap("(function() {<%= contents %>\nreturn exports;\n})()"))
-      .pipe(wrapumd({ namespace: "cyclops" }))
-      .pipe(gulp.dest("dist"))
+gulp.task("js-record", function() {
+  return gulp.src("lib/record/index.js")
+      .pipe(browserify({}))
+      .pipe(rename("cyclops-record.js"))
+      .pipe(gulp.dest("dist"));
 });
+
+gulp.task("js-replay", function() {
+  return gulp.src("lib/replay/index.js")
+      .pipe(browserify({}))
+      .pipe(rename("cyclops-replay.js"))
+      .pipe(gulp.dest("dist"));
+});
+
+// All JS
+gulp.task("js", ["js-record", "js-replay"]);
 
 // Watch Files For Changes
 gulp.task("watch", function() {
--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-var Cyclops = require("./lib/cyclops");
+var webdriver = require("selenium-webdriver");
+var util = require("./lib/common/util");
+var fs = require("fs");
+var path = require("path");
 
-/**
- * var cyclops = require("cyclops").init({
- *   env: {
- *     now: ...
- *     setTimeout: ...
- *   },
- *   recorder: {
- *     buffer: true,
- *     max_buffer_time_ms: 1000,
- *     max_buffer_events: 100
- *   }
- * });
- *
- * cyclops.onEvents(function(events) {
- *
- * });
- *
- * cyclops.config();
- *
- * cyclops.start();
- *
- * cyclops.event({
- *   type: "my_custom_event",
- *   foo: "bar"
- * });
- * cyclops.flush();
- *
- * cyclops.stop();
- *
- */
+var launch = {};
 
-exports.init = function(options) {
-  return Cyclops.init(options);
+launch.launch = function(options) {
+
+  options = util.update({
+    browser: "chrome",
+    webdriver_url: "http://localhost:4444/wd/hub",
+    events_url: null,
+    page_url: null
+  }, options);
+
+  var caps = {
+    "acceptSslCerts": true,
+    "browserName": options.browser
+  };
+
+  console.log("Connecting to webdriver", options.webdriver_url, caps);
+  var driver = new webdriver.Builder()
+      .usingServer(options.webdriver_url)
+      .withCapabilities(caps)
+      .build();
+
+  var promise = driver.getSession().then(function(session) {
+    // The `wdsid` and `wdurl` parameters have names that the WebdriverJS
+    // library looks for.
+    var query_params = {
+      wdsid: session.getId(),
+      wdurl: options.webdriver_url,
+      cyclops_events_url: options.events_url
+    };
+    var url = util.appendQuery(options.page_url, query_params);
+    console.log("Navigating " + caps.browser + " to " + url);
+    return driver.get(url);
+  });
+
+  // If the URL is a local file, read it in and upload it to the JS
+  // once the page is loaded.
+  if (options.events_url.startsWith("file:")) {
+
+    // Read it in
+    var filename = decodeURIComponent(options.events_url.split("://")[1]);
+    var events_json = fs.readFileSync(filename);
+
+    // Send a massive request via webdriver to set it in the JS
+    var deferred = webdriver.promise.defer();
+    promise = promise.then(function() {
+      driver.executeAsyncScript(
+              "var callback = arguments[arguments.length - 1];\n" +
+              "callback();\n" +
+              "var cyclops_data = {};\n" +
+              "cyclops_data.events = JSON.parse(arguments[0]);\n" +
+              "window._$cyclops = cyclops_data;\n",
+          options.events_url,
+          events_json,
+          function() {
+            deferred.resolve();
+          });
+    });
+  }
+
+  return promise;
 };
+
+module.exports = launch;
